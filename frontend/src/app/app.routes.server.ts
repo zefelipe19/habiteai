482f221ed16a5c8b05713befc157c31b8a6ebc2f
José Felipe P. Sousa <felipe.paula1903@gmail.com>
2026-06-17 13:43:39 -0300

    fix: switch the render method to client side

--- frontend/src/app/app.routes.server.ts
+++ frontend/src/app/app.routes.server.ts
@@ -3,6 +3,7 @@ import { RenderMode, ServerRoute } from '@angular/ssr';
 export const serverRoutes: ServerRoute[] = [
   {
     path: '**',
-    renderMode: RenderMode.Prerender
+    // renderMode: RenderMode.Prerender
+    renderMode: RenderMode.Client,
   }
 ];
