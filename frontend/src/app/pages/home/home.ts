e288bca3ae9928d1e668de49c7840d9899bc4364
José Felipe P. Sousa <felipe.paula1903@gmail.com>
2026-06-17 13:12:57 -0300

    feat: update homepage html structure and angular config

--- frontend/src/app/pages/home/home.ts
+++ frontend/src/app/pages/home/home.ts
@@ -1,9 +1,23 @@
 import { Component } from "@angular/core";
+import { Property } from "../../components/property/property"
+import { SearchBox } from "../../components/searchbox/searchbox"
 
 @Component({
   selector: "app-home",
-  imports: [],
+  imports: [Property, SearchBox],
   templateUrl: "./home.html",
   styleUrl: "./home.css",
 })
-export class Home {}
+export class Home {
+  properties = [
+    {
+      id: 1
+    },
+    {
+      id: 2
+    },
+    {
+      id: 3
+    },
+  ]
+}
