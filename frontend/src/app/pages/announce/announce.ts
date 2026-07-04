76323c6fce1bbba48a0f919b8c0ef06d49b68872
José Felipe <felipe.paula1903@gmail.com>
2026-07-04 17:00:32 -0300

    feat: implements the register function in announce page

--- frontend/src/app/pages/announce/announce.ts
+++ frontend/src/app/pages/announce/announce.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
+import { LoginRegister } from '../../components/login-register/login-register';
 
 @Component({
   selector: 'app-announce',
-  imports: [],
+  imports: [LoginRegister],
   templateUrl: './announce.html',
   styleUrl: './announce.css',
 })
-export class Announce {
-
-}
+export class Announce {}
