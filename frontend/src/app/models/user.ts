5d65f0e89679d016e86b199d6321794e4192ceaa
José Felipe P. Sousa <felipe.paula1903@gmail.com>
2026-07-06 15:13:17 -0300

    feat: add basic user models

---
+++ frontend/src/app/models/user.ts
@@ -0,0 +1,14 @@
+export interface baseUser {
+    id: number;
+    email: string;
+    username: string | null;
+    password: string;
+}
+
+type advertiserType = "realState" | "individual"
+
+export interface advertiserUser {
+    userId: number;
+    type: advertiserType;
+}
+
