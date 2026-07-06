30e1700fd1caabb597b6d860cdecc62738301ff8
José Felipe P. Sousa <felipe.paula1903@gmail.com>
2026-07-06 15:11:22 -0300

    feat: implements propertyAnnouncerment interface

--- frontend/src/app/models/propertyModel.ts
+++ frontend/src/app/models/propertyModel.ts
@@ -5,3 +5,12 @@ export interface propertyBase {
     bathrooms: number;
     parkingSpace: number;
 }
+
+type announcementType = "rent" | "sell" | "season"
+
+export interface propertyAnnouncement extends propertyBase {
+    price: number;
+    type: announcementType;
+    announcementId: number;
+    advertiserId: number;
+}
