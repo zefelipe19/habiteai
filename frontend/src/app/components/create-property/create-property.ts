e8e3eea5c8cb97abab9685a16ce1adccb4939246
José Felipe P. Sousa <felipe.paula1903@gmail.com>
2026-07-10 17:03:01 -0300

    feat: implemments the initial structure to create property and announcement

--- frontend/src/app/components/create-property/create-property.ts
+++ frontend/src/app/components/create-property/create-property.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
+import { FormControl, ReactiveFormsModule, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-create-property',
@@ -7,5 +8,19 @@ import { Component } from '@angular/core';
   styleUrl: './create-property.css',
 })
 export class CreateProperty {
+  @Output() propertyCreatedSuccessfully = new EventEmitter<void>()
 
+  propertyForm = new FormGroup({
+    propertyArea: new FormControl<Number>(0),
+    propertyRestrooms: new FormControl<Number>(0),
+    propertyBathrooms: new FormControl<Number>(0),
+    propertyParkingSpace: new FormControl<Number>(0),
+  })
+
+  propertyAnnouncement = new FormGroup({
+    price: new FormControl<Number>(0),
+    type: new FormControl<String>(''),
+    announcementId: new FormControl<Number>(0),
+    advertiserId: new FormControl<Number>(0),
+  })
 }
