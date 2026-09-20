a550812fba5f820d3f7c117459b8ad454d5b5d0c
José Felipe <felipe.paula1903@gmail.com>
2026-09-20 16:33:03 -0300

    feat: updated user model and schemas to get two types of user

--- backend/models.py
+++ backend/models.py
@@ -1,6 +1,13 @@
-from sqlalchemy import String, Boolean
+import enum
+from typing import Optional
+from sqlalchemy import String, Boolean, Enum
 from sqlalchemy.orm import Mapped, mapped_column
 from .database import Base
+
+
+class UserType(str, enum.Enum):
+    PHYSICAL = "PHYSICAL"
+    JURIDICAL = "JURIDICAL"
 
 
 class User(Base):
@@ -10,4 +17,19 @@ class User(Base):
     email: Mapped[str] = mapped_column(
         String(255), unique=True, index=True, nullable=False)
     hashed_password: Mapped[str] = mapped_column(String(255), nullable=False)
+    user_type: Mapped[UserType] = mapped_column(
+        Enum(UserType, native_enum=False, length=20), default=UserType.PHYSICAL)
+    phone: Mapped[str] = mapped_column(String(20))
     is_active: Mapped[bool] = mapped_column(Boolean, default=True)
+
+    # dados para pessoa fisica
+    full_name: Mapped[Optional[str]] = mapped_column(String(150))
+    cpf: Mapped[Optional[str]] = mapped_column(
+        String(14), unique=True, index=True)
+
+    # dados pessoa jurídica
+    company_name: Mapped[Optional[str]] = mapped_column(String(150))
+    trade_name: Mapped[Optional[str]] = mapped_column(String(150))
+    cnpj: Mapped[Optional[str]] = mapped_column(
+        String(18), unique=True, index=True)
+    creci: Mapped[Optional[str]] = mapped_column(String(20))
--- backend/schemas.py
+++ backend/schemas.py
@@ -1,15 +1,62 @@
-from pydantic import BaseModel, EmailStr, ConfigDict
+from enum import Enum
+from typing import Optional
+from pydantic import BaseModel, EmailStr, ConfigDict, Field, model_validator
 
 
-class UserCreate(BaseModel):
+class UserType(str, Enum):
+    PHYSICAL = "PHYSICAL"
+    JURIDICAL = "JURIDICAL"
+
+
+class UserCreateBase(BaseModel):
     email: EmailStr
-    password: str
+    password: str = Field(min_length=6)
+    # padrão para numero de telefone
+    phone: str = Field(pattern=r"^\+?[1-9]\d{1, 14}$")
+
+
+class UserCreatePhysical(UserCreateBase):
+    user_type: UserType = UserType.PHYSICAL
+    full_name: str = Field(min_length=2, max_length=150)
+    cpf: str = Field(min_length=11, max_length=14)
+
+
+class UserCreateJuridical(UserCreateBase):
+    user_type: UserType = UserType.JURIDICAL
+    company_name: str = Field(min_length=2, max_length=150)
+    trade_name: str = Field(min_length=2, max_length=150)
+    cnpj: str = Field(min_length=14, max_length=18)
+    creci: Optional[str] = None
+
+
+class UserUpdate(BaseModel):
+    phone: Optional[str] = None
+
+    # atualização de pf
+    full_name: Optional[str] = None
+
+    # atualização de pj
+    company_name: Optional[str] = None
+    trade_name: Optional[str] = None
+    creci: Optional[str] = None
 
 
 class UserResponse(BaseModel):
     id: int
     email: EmailStr
+    user_type: UserType
+    phone: str
     is_active: bool
+
+    # dados de pessoa fisica
+    full_name: Optional[str] = None
+    cpf: Optional[str] = None
+
+    # dados de pessoa juridica
+    company_name: Optional[str] = None
+    trade_name: Optional[str] = None
+    cnpj: Optional[str] = None
+    creci: Optional[str] = None
 
     model_config = ConfigDict(from_attributes=True)
 
