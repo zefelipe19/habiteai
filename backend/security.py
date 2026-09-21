567384bd70ed593c44c977855688a96d223a03a3
José Felipe <felipe.paula1903@gmail.com>
2026-09-21 16:12:57 -0300

    feat: update get_current_user_email to get_current_user return user object

--- backend/security.py
+++ backend/security.py
@@ -1,9 +1,14 @@
-from datetime import datetime, timedelta, timezone
 import bcrypt
+from datetime import datetime, timedelta, timezone
 from jose import jwt, JWTError
 from fastapi import Depends, HTTPException, status
 from fastapi.security import OAuth2PasswordBearer
+from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy import select
+
 from config import settings
+from database import get_db
+from models import User
 
 SECRET_KEY = settings.SECRET_KEY
 ALGORITHM = settings.ALGORITHM
@@ -32,7 +37,7 @@ def create_access_token(data: dict) -> str:
     return jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
 
 
-def get_current_user_email(token: str = Depends(oauth2_scheme)) -> str:
+async def get_current_user(token: str = Depends(oauth2_scheme), db: AsyncSession = Depends(get_db)) -> User:
     credentials_exception = HTTPException(
         status_code=status.HTTP_401_UNAUTHORIZED,
         detail="Credenciais inválidas",
@@ -43,6 +48,13 @@ def get_current_user_email(token: str = Depends(oauth2_scheme)) -> str:
         email: str = payload.get("sub")
         if email is None:
             raise credentials_exception
-        return email
     except JWTError:
         raise credentials_exception
+
+    result = await db.execute(select(User).where(User.email == email))
+    user = result.scalar_one_or_none()
+
+    if user is None or not user.is_active:
+        raise credentials_exception
+
+    return User
